035e9b572d762fdda45d77b3ced1c06afe6a3516
jackma0625 <jackma0625@gmail.com>
2026-06-15 12:58:44 -0600

    删除bacis的banner图

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -1,119 +1,42 @@
+import { Link } from 'react-router-dom'
 
-import { Link }
-from 'react-router-dom'
-
-export default function BasicLayout({
-
-  restaurant,
-
-}) {
-
+export default function BasicLayout({ restaurant }) {
   return (
-
     <>
-
-
-<Link
-
-  to="/"
-
-  className="back-button"
-
->
-
-  ← Volver
-
-</Link>
-
-
-      <img
-
-        className="restaurant-banner"
-
-        src={restaurant.image}
-
-        alt={restaurant.name}
-
-      />
+      <Link to="/" className="back-button">
+        ← Volver
+      </Link>
 
       <div className="restaurant-details">
-
-        <h1>
-
-          {restaurant.name}
-
-        </h1>
-
-        
+        <h1>{restaurant.name}</h1>
 
         <div className="restaurant-info">
-
           <a
-
             href={restaurant.maps}
-
             target="_blank"
-
             rel="noopener noreferrer"
-
             className="map-link"
-
           >
-
             📍 {restaurant.location}
-
           </a>
-
-          <p>
-
-            🕒 {restaurant.hours}
-
-          </p>
-
+          <p>🕒 {restaurant.hours}</p>
         </div>
-
       </div>
 
       <div className="menu-images">
-
-        {
-
-          restaurant.menuImages?.map((image) => (
-
-            <img
-
-              key={image}
-
-              src={image}
-
-              alt={restaurant.name}
-
-            />
-
-          ))
-
-        }
-
+        {restaurant.menuImages?.map((image) => (
+          <img key={image} src={image} alt={restaurant.name} />
+        ))}
       </div>
 
       <a
-
         href={`https://wa.me/${restaurant.whatsapp}`}
-
         target="_blank"
-
         rel="noopener noreferrer"
-
         className="floating-whatsapp"
-
       >
-
-Ordenar por WhatsApp
-
+        Ordenar por WhatsApp
       </a>
-
     </>
-
   )
-
 }
